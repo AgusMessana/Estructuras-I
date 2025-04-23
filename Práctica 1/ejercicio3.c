2bc458b5ab5d3cfe95a4b006052e6dfd3ae4b7a4
AgusMessana <agustin200369@gmail.com>
2025-04-23 01:28:43 -0300

    Formateo con convenciones establecidas

--- Práctica 1/ejercicio3.c
+++ Práctica 1/ejercicio3.c
@@ -4,45 +4,44 @@
 #include <sys/types.h>
 
 typedef struct {
-int* direccion;
-int capacidad;
+  int *direccion;
+  int capacidad;
 } ArregloEnteros;
 
-ArregloEnteros* arreglo_enteros_crear(int capacidad) {
-    ArregloEnteros* arreglo = (ArregloEnteros*)malloc(sizeof(ArregloEnteros));
-    arreglo -> direccion = (int*)malloc(capacidad * sizeof(int));
-    arreglo -> capacidad = capacidad;
+ArregloEnteros *arreglo_enteros_crear(int capacidad) {
+  ArregloEnteros *arreglo = (ArregloEnteros *) malloc(sizeof(ArregloEnteros));
+  arreglo->direccion = (int *) malloc(capacidad * sizeof(int));
+  arreglo->capacidad = capacidad;
 
-    return arreglo;
+  return arreglo;
 }
 
-void arreglo_enteros_destruir(ArregloEnteros* arreglo) {
-    free(arreglo->direccion);
-    free(arreglo);
+void arreglo_enteros_destruir(ArregloEnteros * arreglo) {
+  free(arreglo->direccion);
+  free(arreglo);
 }
 
-int arreglo_enteros_leer(ArregloEnteros* arreglo, int pos) {
-    if (pos < 0 || pos >= arreglo->capacidad) {
-        return -1;
-    }
-    return(arreglo->direccion[pos]);
+int arreglo_enteros_leer(ArregloEnteros * arreglo, int pos) {
+  if (pos < 0 || pos >= arreglo->capacidad) {
+    return -1;
+  }
+  return (arreglo->direccion[pos]);
 }
 
-void arreglo_enteros_escribir(ArregloEnteros* arreglo, int pos, int dato) {
-    if (pos < 0 || pos >= arreglo->capacidad) {
-        return -1;
-    }
+void arreglo_enteros_escribir(ArregloEnteros * arreglo, int pos, int dato) {
+  if (pos < 0 || pos >= arreglo->capacidad) {
+    return -1;
+  }
 
-    arreglo->direccion[pos] = dato;
+  arreglo->direccion[pos] = dato;
 }
 
-int arreglo_enteros_capacidad(ArregloEnteros* arreglo) {
-    return arreglo->capacidad;
+int arreglo_enteros_capacidad(ArregloEnteros * arreglo) {
+  return arreglo->capacidad;
 }
 
-void arreglo_enteros_imprimir(ArregloEnteros* arreglo) {
-    for (int i = 0; i < arreglo->capacidad; i++) {
-        printf("%d\n", arreglo->direccion[i]);
-    }
+void arreglo_enteros_imprimir(ArregloEnteros * arreglo) {
+  for (int i = 0; i < arreglo->capacidad; i++) {
+    printf("%d\n", arreglo->direccion[i]);
+  }
 }
-
